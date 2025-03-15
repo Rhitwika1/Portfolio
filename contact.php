d91a5bd447bbb49cff4e10c2f0117d4f6c3a458f
Rhitwika Poddar <125289672+Rhitwika1@users.noreply.github.com>
2025-03-15 20:43:58 +0530

    contact.php

--- contact.php
+++ contact.php
@@ -1,5 +1,6 @@
+
 <?php
-	if (isset($_POST["submit"])) {
+	/*if (isset($_POST["submit"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
 		$message = $_POST['message'];
@@ -16,4 +17,30 @@
 	
 	}
 	
+?>*/
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars(strip_tags(trim($_POST['name'])));
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $subject = htmlspecialchars(strip_tags(trim($_POST['subject'])));
+    $message = htmlspecialchars(strip_tags(trim($_POST['message'])));
+
+    $to = 'example@gmail.com'; // Change this to your actual email
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "From: $name\nE-Mail: $email\nSubject: $subject\n\nMessage:\n$message";
+
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if (mail($to, $subject, $body, $headers)) {
+            header("Location: thank-you.html");
+            exit();
+        } else {
+            echo "Mail sending failed. Please try again.";
+        }
+    } else {
+        echo "Invalid email address.";
+    }
+}
 ?>
